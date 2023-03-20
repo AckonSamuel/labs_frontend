90cef7fc8ca5a810150aceed3baefb2dafc12540
Ackon Samuel <ackons433@gmail.com>
2023-03-20 20:54:43 +0000

    Setup routes, cache

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// react-router components
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+// @mui material components
+import {createTheme} from '@mui/material';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/styles';
+import CssBaseline from "@mui/material/CssBaseline";
+
+import routes from "routes";
+
+
+const App = () => {
+
+  const cache = createEmotionCache();
+  const theme = createTheme();
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CacheProvider value={cache}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>{getRoutes(routes)}</Routes>
+    </ThemeProvider>
+    </CacheProvider>
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import SplashScreen from "./components/splashscreen"
+
+const routes = [
+    {
+        name: 'splashscreen',
+        key: 'splashscreen',
+        route: '/welcome',
+        component: <SplashScreen />,
+    }
+];
+
+export default routes;
