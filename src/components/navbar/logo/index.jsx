bf539f2e4166b36a8657b5ca7c90ba0f06b6c693
Ackon Samuel <ackons433@gmail.com>
2023-06-13 12:24:52 +0000

    Add logo

---
+++ src/components/navbar/logo/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+const Logo = () => {
+    return (
+        <Typography variant='h1'>Labs<em>DB</em></Typography>
+    )
+}
+
+export default Logo;
