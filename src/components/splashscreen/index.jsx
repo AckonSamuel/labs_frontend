af3cbb71efee9fdd1329902984d8c2c106c9f54f
Ackon Samuel <ackons433@gmail.com>
2023-06-17 20:01:11 +0000

    Add splashscreen component

--- src/components/splashscreen/index.jsx
+++ src/components/splashscreen/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Copyright from '../../Copyright';
+import Grid from '@mui/material/Grid';
+import Navbar from './../navbar';
+import Button from '@mui/material/Button';
+import Image from 'mui-image';
 
 export default function SplashScreen() {
     let navigate = useNavigate();
@@ -27,73 +24,51 @@ export default function SplashScreen() {
       }
 
   return (
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container sx={{ height: 'auto',
+      background: 'linear-gradient(150deg,  #0DF051, #3E45BC)', }}>
+        <Grid 
+        xs={12}
+        sm={12}
+        md={12}
+        lg={12}
+        xl={12}
+        sx={{ background: 'transparent'}}><Navbar /></Grid>
         <Grid
           item
-          xs={false}
-          sm={4}
-          md={7}
+          lg={6}
           sx={{
-            backgroundImage: 
-            'url(https://user-images.githubusercontent.com/92922987/209251235-962d91f6-12eb-4341-9e71-eaf504965806.jpg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            height: 700,
+            background: 'transparent',
+            padding: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            color: '#fff',
           }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-                 <Box
-        component="img"
+        >
+          <Typography mb={2} component="h1" variant="h1" fontSize={70}>Know your 
+          <br />
+          Lab Equipments</Typography>
+         <Typography mb={7} component="span" variant="body2">
+          Don't be safety blinded, be safety minded
+         </Typography>
+         <Button sx={{
+          background: '#fff',
+          color: '#000',
+          borderRadius: '16px',
+          width: 200,
+         }} variant="contained">Explore Labs DB</Button>
+         </Grid>
+        <Grid 
+        item 
+        xs={12}
+        lg={6}
         sx={{
-          height: 120,
-          width: 80,
-          marginBottom: 9,
-          maxHeight: { xs: 233, md: 167 },
-          maxWidth: { xs: 350, md: 250 },
+          background: 'transparent',
+          borderRadius: '50%',
+          outline: 'solid white',
         }}
-        alt="The house from the offer."
-        src="https://www.freelogovectors.net/wp-content/uploads/2022/03/knust_logo_freelogovectors.net_.png"
-      />
-
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Continue as
-            </Typography>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                id='login-btn'
-                sx={{ mt: 3, mb: 2, backgroundColor: 'green' }}
-                onClick={adminLogin}
-              >
-            Admin
-              </Button>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                id='login-btn'
-                sx={{ mt: 3, mb: 2, backgroundColor: 'green' }}
-                onClick={guestLogin}
-              >
-            Guest
-              </Button>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
+        >
+          {/* <Image src= /> */}
         </Grid>
       </Grid>
   );
