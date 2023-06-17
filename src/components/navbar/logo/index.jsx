720dc6d2f18896a37c01918259647ea9099ed31b
Ackon Samuel <ackons433@gmail.com>
2023-06-17 20:00:24 +0000

    Hot fixes

--- src/components/navbar/logo/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
-const Logo = () => {
-    return (
-        <Typography variant='h1'>Labs<em>DB</em></Typography>
-    )
-}
-
-export default Logo;
