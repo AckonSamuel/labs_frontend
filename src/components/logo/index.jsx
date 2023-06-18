81d6a91f2d1902397bc9949a15fc73aab7f632c7
Ackon Samuel <ackons433@gmail.com>
2023-06-18 23:29:59 +0000

    styling logo component

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 const Logo = () => {
     return (
-        <Typography variant='h1'>Labs<em>DB</em></Typography>
+        <Typography variant='h1' sx={{
+            color: '#fff',
+            fontSize: 30,
+            marginTop: 'auto',
+        }} >Labs <strong>DB</strong></Typography>
     )
 }
 
