b11c15cf6680318329c5b1b0b80e8889403bd9b8
Ackon Samuel <ackons433@gmail.com>
2023-06-21 17:59:10 +0000

    Setup Select category and lab component

---
+++ src/components/select-category-and-lab/index.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Typography from '@mui/material/Typography';
+import MultipleSelectChip from "./select-component";
+
+export default function SelectCategoryAndLab() {
+
+    const sampleCategory = ['power', 'measurement', 'protection', 'soldering'];
+    const sampleLabs = ['mechanical', 'electrical', 'computer', 'electronics'];
+  return (
+        <Box component="form">
+            <Grid container sx={{
+                background: '#fff',
+                borderRadius: '32px',
+                outline: 'solid blue',
+                display: 'flex',
+                padding: 5,
+                margin: 'auto',
+                width: '80%',
+                justifyContent: 'space-around',
+            }}>
+             <Grid item xs={12} sm={12} md={12} lg={12} mb={4}>
+             <Typography variant="title" component="h3" align="center">Enter vendor details</Typography>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} gap={2}
+            sx={{
+              outline: 'solid blue',
+            }}>
+            <Box mb={2}>
+                <MultipleSelectChip label="Category" options={sampleCategory} />
+            </Box>
+            <Box mb={2}>
+            <MultipleSelectChip label="Labs" options={sampleLabs} />
+            </Box>
+            </Grid>
+            </Grid>
+            <Button color="warning">
+              Cancel
+            </Button>
+            <Button color="success" type="submit">
+              Save
+            </Button>
+        </Box>
+  );
+}
