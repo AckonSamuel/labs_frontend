4e19d74268bd640b852518a68b0bb6a8cb9724a7
Ackon Samuel <ackons433@gmail.com>
2023-06-23 23:28:30 +0000

    update redux config file

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
+import addEquipmentReducer from './slices/add-equipment-slice';
 
 const MyMiddlewares = [logger, thunk];
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        addEquipmentReducer,
+    },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(MyMiddlewares),
 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
