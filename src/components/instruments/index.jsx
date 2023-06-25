6d0a7c9e6ea0dbbaa19b7679cc7e6b10f3fe6fd9
Ackon Samuel <ackons433@gmail.com>
2023-06-25 00:00:43 +0000

    Add instrument card component

---
+++ src/components/instruments/index.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import CountryCard from './CountryCard';
+
+const CountryList = ({ countries, searchText, filteredSearch }) => {
+    if (filteredSearch.length === 0 && searchText.length !== 0) {
+        return <h1>Oops! Search not found</h1>;
+    }
+    return (
+<section id="country-list">
+    { countries.length && countries.map(country => (
+        <CountryCard
+        key={country.name.official}
+        officialName={country.name.common}
+        flagsalt={country.flags.alt}
+        flagsvg={country.flags.svg}
+        region={country.region}
+        capital={country.capital}
+        population={country.population}
+        />
+    ))}
+</section>)}
+
+CountryList.propTypes = {
+    countries: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired,
+    filteredSearch: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired,
+    searchText: PropTypes.string.isRequired,
+};
+
+export default CountryList; 
